a3f530d2b1de18f7b088da1fc9cac28b0d2e0bba
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2023-10-03 09:31:49 +0800

    force loading dynamic

--- app/routes/demo/loading/page.tsx
+++ app/routes/demo/loading/page.tsx
@@ -6,6 +6,8 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 
+export const dynamic = "force-dynamic"
+
 export default async function page() {
   // wait 10 secondes
   await new Promise((resolve) => {
